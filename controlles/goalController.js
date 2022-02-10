e22db8ce4a11c5eb55b52543b2bcfee47e275998
KasunHasanga <kasunhasanga96@gmail.com>
2022-02-10 21:52:42 +0530

    connect database and rest apis for goals

--- controlles/goalController.js
+++ controlles/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler =require('express-async-handler')
-
+const Goal =require('../models/goalModel');
 
 // @desc    Get Goals
 // @routes  GET /api/goals
 // @access  Private
 
 const getGoals =asyncHandler( async (req, res) => {
-  res.status(200).json({ Message: "Get goals" });
+  const goals =await Goal.find()
+  res.status(200).json(goals);
 });
 
 // @desc    Set Goal
@@ -20,8 +21,11 @@ const setGoals =asyncHandler( async (req, res) => {
     res.status(400)
     throw new Error("Please add a Text");
   }
+  const goal =await Goal.create({
+    text:req.body.text
+  })
 
-  res.status(200).json({ Message: "Set Goals" });
+  res.status(200).json(goal);
 });
 
 // @desc    update Goal
@@ -29,14 +33,29 @@ const setGoals =asyncHandler( async (req, res) => {
 // @access  Private
 
 const updateGoals =asyncHandler( async (req, res) => {
-  res.status(200).json({ Message: `update Goals ${req.params.id}` });
+
+  const goal =await Goal.findById(req.params.id);
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  const updatedgoal =await Goal.findByIdAndUpdate(req.params.id,req.body,{new:true})
+
+  res.status(200).json(updatedgoal);
+
 });
 // @desc    Delete Goal
 // @routes  DELETE /api/goals
 // @access  Private
 
 const deleteGoals =asyncHandler( async (req, res) => {
-  res.status(200).json({ Message: `Delete Goals ${req.params.id}` });
+  const goal =await Goal.findById(req.params.id);
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+ await Goal.findByIdAndRemove(goal._id)
+  res.status(200).json({ id:  req.params.id });
 });
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const colors =require('colors')
 const dotenv = require("dotenv").config();
 const {errorHandler} =require('./middleware/errorMiddleware')
+const connectDB =require('./config/db')
 var port = process.env.PORT || 3000;
+
+connectDB()
 
 const app = express();
 app.use(express.json());
